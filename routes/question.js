35ce22c6f679aaee71aed0d9da8b25aab8d0e7a9
K kawasumi <g022c1016@g.neec.ac.jp>
2024-10-24 16:04:34 +0900

    10/24

--- routes/question.js
+++ routes/question.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('question', { title: '問題' });
+  let questionsList = []
+  let questions =[]
+  let question = 'test'
+  for (let i = 0; i < 3; i++){
+    questions =[]
+    for (let j = 0; j <= i+1; j++){
+      questions.push(i+1) 
+    }
+    questionsList.push(questions)
+  }
+  res.render('question', { title: '問題',questionsList:questionsList,question:question});
 });
 
 module.exports = router;
